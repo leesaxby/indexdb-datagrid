00fe14dea8d5e696ce41de64d4a8475d1c40a2b6
lee saxby <lee.saxby82@gmail.com>
2015-11-23 06:13:42 -0500

    Add basic auth

--- server.js
+++ server.js
@@ -2,17 +2,18 @@ var exports = module.exports = {};
 
 exports.startServer = function() {
 
+    var auth = require('../auth.js').getAuth();
     var express = require('express');
     var app = express();
     var http = require('http').Server(app);
-    
+
+    app.use(auth);
     app.use(express.static(__dirname + '/public'));
-    
+
     http.listen(80, function(){
       console.log('listening on *:80');
     });
-    
-    return http;
-    
-}
 
+    return http;
+
+}
