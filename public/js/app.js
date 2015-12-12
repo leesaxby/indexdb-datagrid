aa907dcd2781981cc36103e7c2caa5642c85bac1
lee saxby <lee.saxby82@gmail.com>
2015-12-12 11:35:03 -0500

    Added error handling and tidy code.

--- public/js/app.js
+++ public/js/app.js
@@ -3,9 +3,18 @@ var app = app || {}
 app.ViewModel = function() {
   var self = this;
 
+  self.bulkUploadTime = ko.observable(0);
+  self.bulkUploadCount  = ko.observable(0);
+  self.indexTime = ko.observable(0);
+  self.queryTime = ko.observable(0);
+  self.queryCount  = ko.observable(0);
+  self.totalRows  = ko.observable(0);
+  self.records = ko.observableArray([]);
+  self.getOptions = { include_docs: true, limit: 30, descending: false };
+
   self.status = ko.observable(null);
   self.status.subscribe(function( val ) {
-    if( val ) {
+    if ( val ) {
       $('#msgModal').modal('show');
     } else {
       $('#msgModal').modal('hide');
@@ -13,19 +22,30 @@ app.ViewModel = function() {
 
   })
 
-  self.bulkUploadTime = ko.observable(0);
-  self.bulkUploadCount  = ko.observable(0);
-  self.indexTime = ko.observable(0);
-  self.queryTime = ko.observable(0);
-  self.queryCount  = ko.observable(0);
-
-  self.getOptions = { include_docs: true, limit: 60, descending : false };
-  self.records = ko.observableArray([]);
-
   self.nameSearch = ko.observable();
   self.nameSearch.subscribe(function( term ) {
     self.search( term, { name: { $eq: term } } );
   });
+
+  self.errorDialog = function( msg ) {
+    self.status(msg)
+    setTimeout(function() {
+      self.status(null)
+    }, 3000)
+  }
+
+  self.initDocs = function() {
+    self.status('Getting Records...');
+    app.db.allDocs({ limit: 0 }).then(function(data) {
+      if ( data.total_rows > 10000 ) {
+        self.totalRows(data.total_rows)
+        self.getDocs();
+        $('#grid-container').scrollTop(0)
+      } else {
+        self.addAllDocs()
+      }
+    })
+  };
 
   self.search = function( term, searchSelector ) {
 
@@ -124,18 +144,12 @@ app.ViewModel = function() {
   };
 
   self.replaceDoc = function( oldDocId, newDoc ) {
-    for ( var i=0, len = self.records().length - 1; i < len; i++ ) {
+    for ( var i = 0, len = self.records().length - 1; i < len; i++ ) {
       if ( self.records()[i]._id === oldDocId ) {
         self.records.replace( self.records()[i], newDoc );
         break;
       }
     }
-  };
-
-  self.initDocs = function() {
-    self.status('Getting Records...');
-    self.getDocs();
-    $('#grid-container').scrollTop(0)
   };
 
   self.previousRecs = function() {
@@ -146,68 +160,62 @@ app.ViewModel = function() {
     self.getDocs( false );
   };
 
-  self.countDocs = function() {
-    app.db.allDocs().then(function(response){
-      console.log(response.rows.length)
-    }).catch(function(err){
-      console.log(err)
-    })
-  };
-
   self.addAllDocs = function() {
-    app.db.allDocs().then(function(response){
-      if ( response.rows.length < 10000 ) {
-        var data1 = $.ajax( "data/data1.json" ),
-            data2 = $.ajax( "data/data2.json" );
-        self.status('Getting JSON...')
-        $.when( data1, data2 ).then( function( resp1, resp2 ) {
-
-            var allDocs = resp1[0].concat(resp2[0]),
-                startTime = +new Date();
-
-            self.status('Adding Records...')
-            app.db.bulkDocs( allDocs ).then(function( response ) {
-              var endTime = +new Date();
-              self.bulkUploadTime( (endTime - startTime) / 1000 );
-              self.bulkUploadCount( response.length )
-
-              var indexStartTime = +new Date();
-              self.status('Creating Secondary Index...')
-              app.db.createIndex({
-                index: {
-                  fields: ['index', 'name']
-                }
-              }).then(function (result) {
-                var endTime = +new Date();
-                self.indexTime( (endTime - indexStartTime) / 1000 );
-                self.status(null);
-                self.initDocs();
-              }).catch(function (err) {
-                console.log(err)
-                // ouch, an error
-              });
-
-            }).catch(function(err){
-              console.log(err)
-            });
 
 
-        }).fail( function( xhr, err, status ) {
-          console.log(err + ': ' + status)
+    var data1 = $.ajax( "data/data1.json" ),
+        data2 = $.ajax( "data/data2.json" );
+        
+    self.status('Getting JSON...')
+    $.when( data1, data2 ).then( function( resp1, resp2 ) {
+
+       var allDocs = resp1[0].concat(resp2[0]),
+           startTime = +new Date();
+
+      self.status('Adding Records...')
+      app.db.bulkDocs( allDocs ).then(function( response ) {
+
+        var endTime = +new Date();
+        self.bulkUploadTime( (endTime - startTime) / 1000 );
+        self.bulkUploadCount( response.length )
+
+        var indexStartTime = +new Date();
+        self.status('Creating Secondary Index...')
+        app.db.createIndex({
+          index: {
+            fields: [ 'index', 'name' ]
+          }
+        }).then(function(result) {
+          var endTime = +new Date();
+          self.indexTime( (endTime - indexStartTime) / 1000 );
+          self.status(null);
+          self.initDocs();
+        }).catch(function(err) {
+          self.errorDialog('Problem Creating Index...')
+          console.log(err)
         });
-      } else {
-        console.log('already over 100000 docs')
-      }
-    }).catch(function(err){
-      console.log(err)
-    })
+
+      }).catch(function(err) {
+        self.errorDialog('Problem Adding Records...')
+        console.log(err)
+      });
+
+    }).fail( function( xhr, err, status ) {
+      self.errorDialog('Problem Getting JSON...')
+      console.log(err + ': ' + status)
+    });
+
   };
 
   self.deleteDb = function() {
     self.status('Deleting Database...')
     app.db.destroy().then(function() {
-      self.status(null);
+      self.status('Database Deleted...')
+      setTimeout(function() {
+        self.status(null)
+      }, 3000)
     }).catch(function(err) {
+      self.errorDialog('Problem Deleting Database...')
       console.log(err)
     })
   }
